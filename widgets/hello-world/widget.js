a5701a22b3658a3d309fa73fbb68a3813ef28ea2
k-miyagi-dokairen <k.miyagi.dokairen@gmail.com>
2025-07-08 23:10:40 +0900

    Fix: remove duplicate Widget definition

--- widgets/hello-world/widget.js
+++ widgets/hello-world/widget.js
@@ -7,13 +7,4 @@ export default function Widget() {
     </div>
   );
 }
-import { React } from 'jimu-core';
-
-export default function Widget() {
-  return (
-    <div>
-      <h3>Hello World from Custom Widget!</h3>
-    </div>
-  );
-}
 
