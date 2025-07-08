d53c539171fff359349168613d578802e2979106
k-miyagi-dokairen <k.miyagi.dokairen@gmail.com>
2025-07-08 22:09:23 +0900

    初回コミット - Hello World ウィジェット構成

---
+++ widgets/hello-world/config.js
@@ -0,0 +1,5 @@
+cat <<EOF > expb-widget-hello-world/widgets/hello-world/config.js
+export default {
+  message: "This is a config for Hello World widget"
+};
+EOF
---
+++ widgets/hello-world/widget.js
@@ -0,0 +1,3 @@
+cat <<EOF > expb-widget-hello-world/widgets/hello-world/widget.js
+console.log("Hello World Widget Loaded");
+EOF
